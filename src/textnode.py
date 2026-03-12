e7a6bc8f756c8f91be77e37a62e4383466c015b5
MStatev1107 <maxim.statev@gmail.com>
2026-03-12 14:43:27 +0100

    split nodes for links and images

--- src/textnode.py
+++ src/textnode.py
@@ -70,8 +70,62 @@ def extract_markdown_images(text):
     matches = re.findall(pattern, text)
     return matches
 
-
 def extract_markdown_links(text):
     pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern, text)
     return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        text = old_node.text
+        images = extract_markdown_images(text)
+
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        
+        for image_alt, image_link in images:
+            splitted = text.split(f"![{image_alt}]({image_link})", 1)
+            if len(splitted) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if splitted[0] != "":
+                new_nodes.append(TextNode(splitted[0], TextType.TEXT))
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+            
+            text = splitted[1]
+
+        if text != "":
+            new_nodes.append(TextNode(text, TextType.TEXT))                
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        text = old_node.text
+        links = extract_markdown_links(text)
+
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        
+        for link_alt, url_link in links:
+            splitted = text.split(f"[{link_alt}]({url_link})", 1)
+            if len(splitted) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            if splitted[0] != "":
+                new_nodes.append(TextNode(splitted[0], TextType.TEXT))
+            new_nodes.append(TextNode(link_alt, TextType.LINK, url_link))  
+            text = splitted[1]
+        if text != "":
+            new_nodes.append(TextNode(text, TextType.TEXT))               
+    return new_nodes
